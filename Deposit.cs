0a0f4c27bea1f92c7da9518525b34ee51ea30a13
Malin Lindblom <malin.lindblom@edu.edugrade.se>
2023-09-19 09:47:16 +0200

    Nya

---
+++ Deposit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betting
+{
+    internal partial class MyBetting
+    {
+        private void Deposit()
+        {
+            Console.Write("Sätt in pengar");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ MyBetting.cs
@@ -0,0 +1,50 @@
+﻿namespace Betting
+{
+    internal partial class MyBetting
+    {
+ 
+        public void start()
+        {
+            while (true)
+            {
+                //Skapa ett enkelt inlogg???? hej hej 
+                Console.WriteLine("Välkommen till Casono Avengers, välj ett av alternativen");
+                Console.WriteLine("1. spela gissa numret");
+                Console.WriteLine("2. Sätt in pengar");
+                Console.WriteLine("3. Logga ut");
+                Console.Write("Knappa in ditt val : ");
+
+                int input = inputNr();
+                Console.Clear();
+
+                switch (input) 
+                {
+                    case 1:
+                        Guessnr();
+                        break; 
+                    case 2:
+                        Deposit(); 
+                        break;   
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        private int inputNr()
+        {
+            int nr;
+            while (!int.TryParse(Console.ReadLine(), out nr))
+            {
+                Console.WriteLine("inmatnings fel");
+            }
+
+            return nr;
+        }
+    }
+}
